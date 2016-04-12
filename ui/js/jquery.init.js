13223943f344e2a9e4bbaf7d9a57fd559c1752d0
kcroakeJRD <kevin@jumpingjackrabbit.com>
2016-04-12 14:38:40 -0400

    Helpful Junk 1

--- ui/js/jquery.init.js
+++ ui/js/jquery.init.js
@@ -23,19 +23,60 @@ jQuery(function($){
         // if transform3d isn't available, use top over background-position
         //$('#element').css('top', Math.ceil(n/2) + 'px');
 
-    });    
+    });
+*/
+/*
+
+    $('.accordion-tabs').each(function(index) {
+        $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+    });
+    $('.accordion-tabs').on('click', 'li > a.tab-link', function(event) {
+        if (!$(this).hasClass('is-active')) {
+            event.preventDefault();
+            var accordionTabs = $(this).closest('.accordion-tabs');
+            accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+            $(this).next().toggleClass('is-open').toggle();
+            accordionTabs.find('.is-active').removeClass('is-active');
+            $(this).addClass('is-active');
+        } else {
+            event.preventDefault();
+        }
+    });
+
+
+*/
+/* Mobile Menu
+$('.nav-toggle').click(function(){
+                $('#nav-container').toggleClass('show');
+    });
+
+    // Toggled label
+    var tt = "Close";
+    $('.nav-toggle').click(function(){
+       var tn = $(this).text();
+       $(this).toggleClass('close').text(tt);
+       tt = tn;
+       $('#mobile-menu').slideToggle();
+    });
+
+
+
+    $('.menu-item-has-children span').click(function(){
+           $(this).parent().toggleClass('open');
+           $(this).parent().parent().toggleClass('open');
+           $(this).parent().next('ul').slideToggle(200);
+    });
 */
 
-
-
-    /* ====== Twitter API Call ============================================= 
+    /* ====== Twitter API Call =============================================
         Note: Script Automatically adds <li> before and after template. Don't forget to setup Auth info in /twitter/index.php */
     /*
-    $('#tweets-loading').tweet({       
+    $('#tweets-loading').tweet({
         modpath: '/path/to/twitter/', // only needed if twitter folder is not in root
         username: 'jackrabbits',
         count: 1,
-		template: '<p>{text}</p><p class="tweetlink">{time}</p>' 
+		template: '<p>{text}</p><p class="tweetlink">{time}</p>'
 	});
     */
 
