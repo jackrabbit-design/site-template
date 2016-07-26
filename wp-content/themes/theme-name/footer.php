f2ade667ace1fb8bb446e8d96b4c269251fd7bae
Chris Kirk <ck@jumpingjackrabbit.com>
2016-07-26 09:31:20 -0400

    Jason is forgetful.

---
+++ wp-content/themes/theme-name/footer.php
@@ -0,0 +1,6 @@
+
+    <script src="../ui/js/jquery.plugins.js" type="text/javascript"></script>
+    <script src="../ui/js/jquery.init.js" type="text/javascript"></script>
+    <?php wp_footer(); ?>
+</body>
+</html>
