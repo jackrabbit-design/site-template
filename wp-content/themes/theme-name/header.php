583e1845326f5a2639f37fb32e7fe0c1a9174b95
Chris Kirk <ck@jumpingjackrabbit.com>
2016-08-04 08:53:37 -0400

    remove scripts
    style, jquery, and modernizr are already being enqueued in functions.php

--- wp-content/themes/theme-name/header.php
+++ wp-content/themes/theme-name/header.php
@@ -32,11 +32,8 @@
     <meta name="MSSmartTagsPreventParsing" content="true" /><!--[if lte IE 9]><meta http-equiv="X-UA-Compatible" content="IE=Edge"/><![endif]-->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
     <title>Title</title>
-	<link type="text/css" rel="stylesheet" media="all" href="../ui/css/style.css" />
     <link type="text/plain" rel="author" href="../authors.txt" />
     <link type="image/x-icon" rel="shortcut icon" href="../favicon.ico" />
-    <script src="../ui/js/modernizr.js"></script>
-    <script src="../ui/js/jquery.js" type="text/javascript"></script>
     <?php wp_head(); ?>
 </head>
 <body>
