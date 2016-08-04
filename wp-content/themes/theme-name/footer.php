abd91db4ebabb86cdf456e010fab43b34761f87c
Chris Kirk <ck@jumpingjackrabbit.com>
2016-08-04 08:54:05 -0400

    remove scripts
    plugins and init are already being enqueued in functions.php

--- wp-content/themes/theme-name/footer.php
+++ wp-content/themes/theme-name/footer.php
@@ -1,6 +1,4 @@
 
-    <script src="../ui/js/jquery.plugins.js" type="text/javascript"></script>
-    <script src="../ui/js/jquery.init.js" type="text/javascript"></script>
     <?php wp_footer(); ?>
 </body>
 </html>
