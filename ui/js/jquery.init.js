b0936ada5c812ec2dd58c8fe19d0c82d2a5a2b29
jplarose80 <jason@jumpingjackrabbit.com>
2016-09-16 17:11:40 -0400

    Updated stuff.
    - removed unsupported.html and call from index.html
    - added svg4everybody script to jquery file and init file.

--- ui/js/jquery.init.js
+++ ui/js/jquery.init.js
@@ -2,6 +2,8 @@
 /* BE SURE TO COMMENT CODE/IDENTIFY PER PLUGIN CALL */
 /* ========================================================================= */
 
+svg4everybody();
+
 jQuery(function($){
 
 
@@ -23,19 +25,19 @@ jQuery(function($){
         // if transform3d isn't available, use top over background-position
         //$('#element').css('top', Math.ceil(n/2) + 'px');
 
-    });    
+    });
 */
 
 
 
-    /* ====== Twitter API Call ============================================= 
+    /* ====== Twitter API Call =============================================
         Note: Script Automatically adds <li> before and after template. Don't forget to setup Auth info in /twitter/index.php */
     /*
-    $('#tweets-loading').tweet({       
+    $('#tweets-loading').tweet({
         modpath: '/path/to/twitter/', // only needed if twitter folder is not in root
         username: 'jackrabbits',
         count: 1,
-		template: '<p>{text}</p><p class="tweetlink">{time}</p>' 
+		template: '<p>{text}</p><p class="tweetlink">{time}</p>'
 	});
     */
 
