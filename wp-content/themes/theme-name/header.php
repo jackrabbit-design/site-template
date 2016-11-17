c0d1aa87ce08550e10096e6c6fad08a7dfa50c0d
Doug Gerber <dojoh@outlook.com>
2016-11-17 10:55:42 -0500

    Using wp_title
    title tag updated to use wp_title();

--- wp-content/themes/theme-name/header.php
+++ wp-content/themes/theme-name/header.php
@@ -31,7 +31,7 @@
 -->
     <meta name="MSSmartTagsPreventParsing" content="true" /><!--[if lte IE 9]><meta http-equiv="X-UA-Compatible" content="IE=Edge"/><![endif]-->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-    <title>Title</title>
+    <title><?php wp_title(); ?></title>
     <link type="text/plain" rel="author" href="../authors.txt" />
     <link type="image/x-icon" rel="shortcut icon" href="../favicon.ico" />
     <?php wp_head(); ?>
