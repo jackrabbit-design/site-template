3af0302447dd48749cb6c41c32d45cdf2a7ec396
Chris Kirk <ck@jumpingjackrabbit.com>
2019-05-13 15:53:52 -0400

    4.2.19
    jQuery ⬆️ 3.4.1
    Remove viewport scale disabling

--- wp-content/themes/theme-name/header.php
+++ wp-content/themes/theme-name/header.php
@@ -8,27 +8,27 @@
                                                        3333
                                                       '3333
                  :333:                       ,;';,    3333;
-                 3333      '33333333+    ,3333333333,:3333 
-                33333    ,333333'+333   33333+`  .33333333 
-                3333:   .3333`        .3333'        33333  
-               ;3333    3333:         3333+         33333  
-               3333+   ,3333         ;3333          3333   
-              .3333    3333'         +3333         33333   
-              33333   `3333           3333+       +3333    
-              3333    33333            333333;;'33333+     
-             33333    3333                33333333         
-    +.     ;33333                                          
-     +333333333:                                           
-        +3+'                                               
+                 3333      '33333333+    ,3333333333,:3333
+                33333    ,333333'+333   33333+`  .33333333
+                3333:   .3333`        .3333'        33333
+               ;3333    3333:         3333+         33333
+               3333+   ,3333         ;3333          3333
+              .3333    3333'         +3333         33333
+              33333   `3333           3333+       +3333
+              3333    33333            333333;;'33333+
+             33333    3333                33333333
+    +.     ;33333
+     +333333333:
+        +3+'
 
 
     Website created by Jackrabbit | jumpingjackrabbit.com
     ===================================================== -->
     <meta name="MSSmartTagsPreventParsing" content="true" /><!--[if lte IE 9]><meta http-equiv="X-UA-Compatible" content="IE=Edge"/><![endif]-->
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title><?php wp_title(); ?></title>
-    <link type="text/plain" rel="author" href="../authors.txt" />
-    <link type="image/x-icon" rel="shortcut icon" href="../favicon.ico" />
+    <link type="text/plain" rel="author" href="/authors.txt" />
+    <link type="image/x-icon" rel="shortcut icon" href="/favicon.ico" />
     <?php
     wp_head();
     ?>
