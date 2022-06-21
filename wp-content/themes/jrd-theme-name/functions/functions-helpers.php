d26a7b80fa59efe011eacbeb7d4287e2d214179d
Chris Kirk <ck@jumpingjackrabbit.com>
2022-06-21 17:08:52 -0400

    5.4.4
    Prep for Link2

--- wp-content/themes/jrd-theme-name/functions/functions-helpers.php
+++ wp-content/themes/jrd-theme-name/functions/functions-helpers.php
@@ -133,11 +133,11 @@ function jrd_img( $field, $size = 'large', $classes = null, $id = null, $data = 
  */
 function jrd_link( $link, $class = '', $id = '' ) {
 	if ( $link ) {
-		$link_title       = $link['title'] ? $link['title'] : $link['url'];
-		$link_title_clean = wp_strip_all_tags( $link_title );
-		$link_url         = esc_url( $link['url'] );
-		$target           = $link['target'] ?? '_self';
-		return "<a href='{$link_url}' title='{$link_title_clean}' target='{$target}' class='$class' id='$id'><span>{$link['title']}</span></a>" . PHP_EOL;
+		$link_label = isset( $link['label'] ) && '' !== (string) $link['label'] ? $link['label'] : $link['title'];
+		$link_url   = esc_url( $link['url'] );
+		$target     = $link['target'] ?? '_self';
+		$nofollow   = isset( $link['nofollow'] ) && 'nofollow' === $link['nofollow'] ? "rel='nofollow'" : '';
+		return "<a href='{$link_url}' title='{$link_label}' target='{$target}' class='$class' id='$id' $nofollow><span>{$link['title']}</span></a>" . PHP_EOL;
 	}
 }
 
