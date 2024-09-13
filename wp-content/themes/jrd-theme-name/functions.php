21fe47d3d615cd2ebd190b911ed34287ab97e5d6
Chris Kirk <5385895+ck-jrd@users.noreply.github.com>
2024-09-13 10:43:15 -0400

    7.4.4

--- wp-content/themes/jrd-theme-name/functions.php
+++ wp-content/themes/jrd-theme-name/functions.php
@@ -67,6 +67,28 @@ function replace_wp_headers( $headers ) {
 add_filter( 'wp_headers', 'replace_wp_headers' );
 
 /* ========================================================================= */
+/* Redirect attachment post to parent post */
+/* ========================================================================= */
+
+function attachment_redirect() {
+	global $post;
+	if ( ! is_attachment() || ! isset( $post->post_parent ) || ! is_numeric( $post->post_parent ) ) {
+		return;
+	}
+	// Does the attachment have a parent post?
+	// If the post is trashed, fallback to redirect to homepage.
+	if ( 0 !== $post->post_parent && 'publish' === get_post_status( $post->post_parent ) ) {
+		// Redirect to the attachment parent.
+		wp_safe_redirect( get_permalink( $post->post_parent ), 301 );
+	} else {
+		// For attachment without a parent redirect to homepage.
+		wp_safe_redirect( home_url(), 302 );
+	}
+	exit;
+}
+add_action( 'template_redirect', 'attachment_redirect', 1 );
+
+/* ========================================================================= */
 /* WordPress External Files     */
 /* ========================================================================= */
 
