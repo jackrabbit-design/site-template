7d500daa3d1ee6a6e8ebc5695e2e3365ae302203
Chris Kirk <5385895+ck-jrd@users.noreply.github.com>
2024-09-26 14:00:45 -0400

    6.6.7

--- wp-content/themes/jrd-theme-name/functions/functions-comments.php
+++ wp-content/themes/jrd-theme-name/functions/functions-comments.php
@@ -4,34 +4,36 @@
 /* !WordPress Comments HTML Function */
 /* ========================================================================= */
 
-function jrd_comments( $comment, $args, $depth ) {
-	$GLOBALS['comment'] = $comment;
-	//print_r($comment);
+if ( ! function_exists( 'jrd_comments' ) ) {
+	function jrd_comments( $comment, $args, $depth ) {
+		$GLOBALS['comment'] = $comment;
+		//print_r($comment);
 
-	/* Check if post is by Author for special styling */
-	$is_by_author = false;
-	if ( get_the_author_meta( 'email' ) === $comment->comment_author_email ) {
-		$is_by_author = true;
+		/* Check if post is by Author for special styling */
+		$is_by_author = false;
+		if ( get_the_author_meta( 'email' ) === $comment->comment_author_email ) {
+			$is_by_author = true;
+		}
+		?>
+
+		<li>
+			<h4><span>
+				<?php
+				// translators: USERNAME wrote:
+				printf( __( '%s wrote:', 'jrd-theme-name' ), $comment->comment_author );
+				?>
+				</span>
+			</h4>
+			<?php comment_text(); ?>
+			<p class="commentDate">
+				<?php
+				// translators: Written on DATE at TIME
+				printf( __( 'Written on %1$s at %2$s', 'jrd-theme-name' ), get_comment_date( 'n/j/Y' ), get_comment_time( 'g:ia' ) );
+				?>
+				<?php edit_comment_link( __( 'Edit', 'jrd-theme-name' ), '  ', '' ); ?>
+			</p>
+		</li>
+
+		<?php
 	}
-	?>
-
-	<li>
-		<h4><span>
-			<?php
-			// translators: USERNAME wrote:
-			printf( __( '%s wrote:', 'jrd-theme-name' ), $comment->comment_author );
-			?>
-			</span>
-		</h4>
-		<?php comment_text(); ?>
-		<p class="commentDate">
-			<?php
-			// translators: Written on DATE at TIME
-			printf( __( 'Written on %1$s at %2$s', 'jrd-theme-name' ), get_comment_date( 'n/j/Y' ), get_comment_time( 'g:ia' ) );
-			?>
-			<?php edit_comment_link( __( 'Edit', 'jrd-theme-name' ), '  ', '' ); ?>
-		</p>
-	</li>
-
-	<?php
 }
