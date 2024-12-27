47d870ee27e4b2439d7b24981d02123a0c55d91b
Chris Kirk <5385895+ck-jrd@users.noreply.github.com>
2024-12-27 09:04:38 -0500

    7.4.12

---
+++ wp-content/themes/jrd-theme-name/blocks/new.php
@@ -0,0 +1,90 @@
+<?php
+/*
+
+To use:
+1. Open the terminal app of your choice
+2. ls to the "blocks" directory of this theme
+3. enter "php new.php"
+4. Type the slug and title for the new block
+
+*/
+
+print( 'Enter the SLUG of the block: ' );
+$slug = fopen( 'php://stdin', 'r' );
+$slug = rtrim( fgets( $slug ) );
+
+print( 'Enter the TITLE of the block: ' );
+
+$title = fopen( 'php://stdin', 'r' );
+$title = rtrim( fgets( $title ) );
+
+mkdir( $slug );
+file_put_contents(
+	"./$slug/block.json",
+	'{
+    "name": "jrd/' . $slug . '",
+    "title": "' . $title . '",
+    "description": "",
+    "category": "",
+    "icon": "block-default",
+    "keywords": [],
+    "acf": {
+        "mode": "preview",
+        "renderTemplate": "' . $slug . '.php",
+        "postTypes": []
+    },
+    "align": "full",
+    "supports": {
+        "align": false,
+        "align_text": false,
+        "align_content": false,
+        "full_height": false,
+        "mode": false,
+        "multiple": true
+    },
+    "example": {
+        "attributes": {
+            "mode": "preview",
+            "data": {
+                "_is_preview": true
+            }
+        }
+    }
+}'
+);
+
+file_put_contents(
+	"./$slug/$slug.php",
+	'<?php
+/**
+ * Block Name: ' . $title . '
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during backend preview render.
+ * @param   int $post_id The post ID the block is rendering content against.
+ *          This is either the post ID currently being displayed inside a query loop,
+ *          or the post ID of the post hosting this block.
+ * @param   array $context The context provided to the block by the post or it\'s parent block.
+ */
+
+if ( ! empty( $block[\'data\'][\'_is_preview\'] ) ) {
+	echo block_preview( __DIR__ );
+	return false;
+}
+
+// global $post;
+
+// Create class attribute allowing for custom "className" value.
+$class_name = \'\';
+if ( ! empty( $block[\'className\'] ) ) {
+	$class_name .= \' \' . $block[\'className\'];
+}
+?>
+
+<section class=\'<?php echo esc_attr( $class_name ); ?>\'>
+
+</section>'
+);
+
+copy( './_example/preview.jpg', "./$slug/preview.jpg" );
